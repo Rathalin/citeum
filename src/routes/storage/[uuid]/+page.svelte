b800461818088b04ca950ef9e2c551a8e29b9645
Rathalin <danielflo1997@hotmail.com>
2025-06-09 10:29:28 +0200

    Update theme

--- src/routes/storage/[uuid]/+page.svelte
+++ src/routes/storage/[uuid]/+page.svelte
@@ -2,13 +2,17 @@
 	import type { PageData } from './$types'
 
 	let { data }: { data: PageData } = $props()
+
+	let productCountText = $derived(
+		` (${data.products.length} ${data.products.length === 1 ? 'Produkt' : 'Produkte'})`,
+	)
 </script>
 
 <section>
-	<h1>{data.storage.name} ({data.products.length} Produkte)</h1>
+	<h1>{data.storage.name}{productCountText}</h1>
 	<ul>
 		{#each data.products as product (product.id)}
-			<li>{product.name}</li>
+			<li>{product.name} - {product.bestBeforeDate ?? '/'}</li>
 		{/each}
 	</ul>
 </section>
